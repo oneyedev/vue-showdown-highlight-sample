a66dbab87cd0bc0aca4d964a077c397c83c17e6e
oneyedev <oneyedev@gmail.com>
2019-04-09 12:56:02 +0900

    Add custom extensions

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,25 @@
       :markdown="markdown"
       flavor="github"
       class="markdown-body"
-      :extensions="['showdownHighlight']"
+      :extensions="['showdownHighlight', 'colorTextExtension', 'h1LinkExtension']"
     ></vue-showdown>
   </div>
 </template>
 
 <script>
 const MARKDOWN_SAMPLE = `
+# Welcome to vue-showdown-hightlight sample page!
+This page source is markdown. Belows are made by \`vue-showdown\`, \`showdown-hightlight\` module in \`vue-framework\`.
+
+You can see h1 tag link like github. All steps are here - [Convert Markdown Text into HTML (vue-showdown-highlight)](https://oneyedev.github.io/#/articles/convert-markdown-text-into-html-1)
+
 # This is a h1
 ## This is a h2 
 ### This is a h3
 #### This is a h4
 ##### This is a h5
 ###### this is a h6
-***
+# List item
 - This is a unordered item1
 - This is a unordered item2
 - This is a unordered item3
@@ -30,23 +35,31 @@ const MARKDOWN_SAMPLE = `
  * depth2
    * depth3 is not working! (maybe.. bug?)
 
-***
+# Link, Image
 [This is a hyper link](https://google.com)
 ![This is a image](https://picsum.photos/200/200/?random)
 ![This is a image alt](https://)
-***
 
+# Text 
 <center>This text is centered</center>
-**This is a emphasizing text**
 *This is a italic-style text*
+**This is a strong text**
+
 > This is a block quote
 
+# Code block
 \`\`\`sh
 This is a code block
 \`\`\`
+
 \`\`\`js
 const HELLO_WORLD = 'Hello World!'
 \`\`\`
+
+# Custom extension
+***[#FF0000]This is a color text by a custom extension***
+Links are added at h1 tag by a custom extension. click it!
+
 `;
 export default {
   name: "app",
--- src/plugins/vue-showdown.js
+++ src/plugins/vue-showdown.js
@@ -4,6 +4,27 @@ import 'github-markdown-css'
 
 import showdownHighlight from 'showdown-highlight'
 import 'highlight.js/styles/github.css'
-showdown.extension('showdownHighlight', showdownHighlight)
 
+
+const colorTextExtension = {
+    type: 'lang', // define new syntax
+    regex: /\*\*\*\[(#[0-9A-F]{6})\](.+)\*\*\*/g,
+    replace: '<span style="color:$1">$2</span>'
+};
+let x = 0;
+const h1LinkExtension = {
+    type: 'output', // add feature to existing syntax
+    filter: function (text, converter) {
+        if (x < 1) {
+            ++x
+            const html = converter.makeHtml(text)
+            return html.replace(/<h1 id="(.+)">(.+)<\/h1>/g, `<h1 id="$1"><a href="#$1" class="anchor" aria-hidden="true"><i style="visibility: visible" class="octicon octicon-link material-icons">link</i></a>$2</h1>`)
+        }
+        return text;
+    }
+}
+
+showdown.extension('showdownHighlight', showdownHighlight)
+showdown.extension('colorTextExtension', colorTextExtension)
+showdown.extension('h1LinkExtension', h1LinkExtension)
 Vue.use(VueShowdown)
